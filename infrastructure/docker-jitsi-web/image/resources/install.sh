0900db54b8f05a333dcb66f7acc59aba5a9ebb6a
see <erik.seiert@meissa-gmbh.de>
2022-04-08 16:20:30 +0200

    deleted entrypoint, changed install.sh

--- infrastructure/docker-jitsi-web/image/resources/install.sh
+++ infrastructure/docker-jitsi-web/image/resources/install.sh
@@ -4,4 +4,4 @@ set -Eeo pipefail
 apt update > /dev/null
 
 install -m 0700 /tmp/install-debug.sh /usr/local/bin/
-install -m 0755 /tmp/entrypoint.sh /
+install -m 0644 /tmp/settings-config.js /defaults/settings-config.js
