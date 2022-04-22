eff955eb23d6f813f924336848ea3f07396ecbd3
see <erik.seiert@meissa-gmbh.de>
2022-04-22 14:32:01 +0200

    config comments

--- infrastructure/docker-coturn/image/resources/entrypoint.sh
+++ infrastructure/docker-coturn/image/resources/entrypoint.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 
-#function main() {
-#    file_env POSTGRES_DB
-#    file_env POSTGRES_PASSWORD
-#    file_env POSTGRES_USER
-#
-#    create-pg-pass
-#
-#    /usr/local/bin/backup.sh
-#}
-#
-#source /usr/local/lib/functions.sh
-#main
