ac7807bea43437abe06cea90c694def0b06c3957
see <erik.seiert@meissa-gmbh.de>
2022-05-20 11:12:18 +0200

    deleted coturn

--- infrastructure/docker-coturn/image/resources/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -Eeo pipefail
-
-apt update > /dev/null
-
-install -m 0700 /tmp/install-debug.sh /usr/local/bin/
-install -m 0600 /tmp/turnserver.conf /etc/coturn/turnserver.conf
-install -m 0700 /tmp/entrypoint.sh /entrypoint.sh
