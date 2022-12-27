c75f113e71a11fa4aa831c8a7b1a3a5f3dcd269a
jerger <michael.jerger@meissa-gmbh.de>
2022-12-27 20:40:31 +0100

    Version 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-jitsi "1.3.0-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-jitsi "1.3.0"
   :description "jitsi c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
