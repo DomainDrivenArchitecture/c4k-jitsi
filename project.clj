98eadc7e8192159cc7a440c23e1213e047db97a4
Michael Jerger <michael.jerger@meissa-gmbh.de>
2023-08-17 17:46:10 +0200

    bump version to: 1.3.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-jitsi "1.3.5"
+(defproject org.domaindrivenarchitecture/c4k-jitsi "1.3.6-SNAPSHOT"
   :description "jitsi c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
