2bbfd0f0a671c20831edecc4317c8eb0e23a1edb
Clemens <clemens.geibel@meissa-gmbh.de.de>
2023-09-22 11:37:58 +0200

    bump version to: 1.4.1-SNAPSHOT

--- infrastructure/app/build.py
+++ infrastructure/app/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = "c4k-jitsi"
 MODULE = "app"
 PROJECT_ROOT_PATH = "../.."
-version = "1.4.0"
+version = "1.4.1-SNAPSHOT"
 
 
 @init
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-jitsi "1.4.0"
+(defproject org.domaindrivenarchitecture/c4k-jitsi "1.4.1-SNAPSHOT"
   :description "jitsi c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
