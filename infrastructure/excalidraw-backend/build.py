04cfc0c6b1eadf16de2761d1ef26181cb3dd929e
Clemens <clemens.geibel@meissa-gmbh.de.de>
2023-09-29 10:25:18 +0200

    release: 1.5.0

--- infrastructure/excalidraw-backend/build.py
+++ infrastructure/excalidraw-backend/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = "c4k-jitsi"
 MODULE = "excalidraw-backend"
 PROJECT_ROOT_PATH = "../.."
-version = "1.4.2-SNAPSHOT"
+version = "1.5.0"
 
 
 @init
--- infrastructure/web/build.py
+++ infrastructure/web/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = "c4k-jitsi"
 MODULE = "web"
 PROJECT_ROOT_PATH = "../.."
-version = "1.4.3-SNAPSHOT"
+version = "1.5.0"
 
 
 @init
