83f0b636852f6e5376499b6349cd7d2a11e5c23f
Mirco <mirco.zachmann@meissa.de>
2023-11-30 21:33:14 +0100

    release: 2.0.0

--- infrastructure/excalidraw-backend/build.py
+++ infrastructure/excalidraw-backend/build.py
@@ -6,7 +6,7 @@ from ddadevops import *
 name = "c4k-jitsi"
 MODULE = "excalidraw-backend"
 PROJECT_ROOT_PATH = "../.."
-version = "1.6.1-SNAPSHOT"
+version = "2.0.0"
 
 
 @init
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-jitsi "1.6.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-jitsi "2.0.0"
   :description "jitsi c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
