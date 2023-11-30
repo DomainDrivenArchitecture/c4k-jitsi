6a5586d2a824448d03e8b6925fa6f466be771e5d
Mirco <mirco.zachmann@meissa.de>
2023-11-30 21:31:52 +0100

    further improvements to build docker image

--- infrastructure/excalidraw-backend/image/resources/install.sh
+++ infrastructure/excalidraw-backend/image/resources/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -eux
+set -exo pipefail
 
 apt-get update > /dev/null
-apt-get upgrade -y > /dev/null
+apt-get -y upgrade > /dev/null
 apt-get clean
+rm -rf /var/lib/apt/lists/*
 
-npm install -g npm@latest
 npm ci --omit=dev
 npm run build
--- infrastructure/web/image/resources/install.sh
+++ infrastructure/web/image/resources/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -eux
+set -exo pipefail
+
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
 apt-get update > /dev/null
-apt-get upgrade -y > /dev/null
+apt-get -qy upgrade > /dev/null
 apt-get clean
+rm -rf /var/lib/apt/lists/*
 
 
 install -m 0700 /tmp/install-debug.sh /usr/local/bin/
