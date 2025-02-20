e51b114bef90bc2fce66bee36fb28a36d11a9e73
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-02-20 17:11:12 +0100

    fix excalidraw ci build

--- src/main/cljs/dda/c4k_jitsi/browser.cljs
+++ src/main/cljs/dda/c4k_jitsi/browser.cljs
@@ -1,11 +1,9 @@
 (ns dda.c4k-jitsi.browser
   (:require
    [clojure.tools.reader.edn :as edn]
-   [dda.c4k-common.monitoring :as mon]
    [dda.c4k-common.common :as cm]
    [dda.c4k-common.browser :as br]
-   [dda.c4k-jitsi.core :as core]
-   [dda.c4k-jitsi.jitsi :as jitsi]))
+   [dda.c4k-jitsi.core :as core]))
 
 (defn generate-content []
   (cm/concat-vec
@@ -13,22 +11,22 @@
       (br/generate-needs-validation) :content
       (cm/concat-vec
        (br/generate-group
-        "domain"
-        (cm/concat-vec
-         (br/generate-input-field "fqdn" "Your fqdn:" "jitsi.prod.meissa-gmbh.de")
-         (br/generate-input-field "issuer" "(Optional) Your issuer prod/staging:" "")
-         (br/generate-input-field "mon-cluster-name" "(Optional) monitoring cluster name:" "jitsi")
-         (br/generate-input-field "mon-cluster-stage" "(Optional) monitoring cluster stage:" "test")
-         (br/generate-input-field "mon-cloud-url" "(Optional) grafana cloud url:" "https://prometheus-prod-01-eu-west-0.grafana.net/api/prom/push")
-         ))
+        "config"
+        (br/generate-text-area
+         "config" "Your config.edn:"
+         "{:fqdn \"jitsi.your.domain\"
+ :mon-cfg {:cluster-name \"jitsi\"
+                 :cluster-stage \"test\"
+                 :grafana-cloud-url \"https://prometheus-prod-01-eu-west-0.grafana.net/api/prom/push\"}}"
+         "5"))
        (br/generate-group
-        "credentials"
+        "auth"
         (br/generate-text-area "auth" "Your auth.edn:" "{:jvb-auth-password \"jitsi\"
  :jicofo-auth-password \"jicofo-password\"
  :jicofo-component-secret \"jicofo-component-secrect\"
  :mon-auth {:grafana-cloud-user \"your-user-id\"
             :grafana-cloud-password \"your-cloud-password\"}}}"
-        "5"))
+        "6"))
        [(br/generate-br)]
        (br/generate-button "generate-button" "Generate c4k yaml")))]
     (br/generate-output "c4k-jitsi-output" "Your c4k deployment.yaml:" "25")))
@@ -40,27 +38,8 @@
    :content
    (generate-content)})
 
-(defn config-from-document []
-  (let [issuer (br/get-content-from-element "issuer" :optional true)
-        mon-cluster-name (br/get-content-from-element "mon-cluster-name" :optional true)
-        mon-cluster-stage (br/get-content-from-element "mon-cluster-stage" :optional true)
-        mon-cloud-url (br/get-content-from-element "mon-cloud-url" :optional true)]
-    (merge
-     {:fqdn (br/get-content-from-element "fqdn")}     
-     (when (some? issuer)
-       {:issuer issuer})
-     (when (some? mon-cluster-name)
-       {:mon-cfg {:cluster-name mon-cluster-name
-                  :cluster-stage (keyword mon-cluster-stage)
-                  :grafana-cloud-url mon-cloud-url}})
-     )))
-
 (defn validate-all! []
-  (br/validate! "fqdn" ::jitsi/fqdn)  
-  (br/validate! "issuer" ::jitsi/issuer :optional true)
-  (br/validate! "mon-cluster-name" ::mon/cluster-name :optional true)
-  (br/validate! "mon-cluster-stage" ::mon/cluster-stage :optional true)
-  (br/validate! "mon-cloud-url" ::mon/grafana-cloud-url :optional true)
+  (br/validate! "config" core/config? :deserializer edn/read-string)
   (br/validate! "auth" core/auth? :deserializer edn/read-string)
   (br/set-form-validated!))
 
@@ -68,22 +47,22 @@
   (-> (br/get-element-by-id name)
       (.addEventListener "blur" #(do (validate-all!)))))
 
-
 (defn init []
   (br/append-hickory (generate-content-div))
-  (-> js/document
-      (.getElementById "generate-button")
-      (.addEventListener "click"
-                         #(do (validate-all!)
-                              (-> (cm/generate-common
-                                   (config-from-document)
-                                   (br/get-content-from-element "auth" :deserializer edn/read-string)
-                                   {}
-                                   core/k8s-objects)
-                                  (br/set-output!)))))
-  (add-validate-listener "fqdn")
-  (add-validate-listener "issuer")
-  (add-validate-listener "mon-cluster-name")
-  (add-validate-listener "mon-cluster-stage")
-  (add-validate-listener "mon-cloud-url")
-  (add-validate-listener "auth"))
+  (let [config-only false
+        auth-only false]
+    (-> js/document
+        (.getElementById "generate-button")
+        (.addEventListener "click"
+                           #(do (validate-all!)
+                                (-> (cm/generate-cm
+                                     (br/get-content-from-element "config" :deserializer edn/read-string)
+                                     (br/get-content-from-element "auth" :deserializer edn/read-string)
+                                     core/config-defaults
+                                     core/config-objects
+                                     core/auth-objects
+                                     config-only
+                                     auth-only)
+                                    (br/set-output!)))))
+    (add-validate-listener "config")
+    (add-validate-listener "auth")))
